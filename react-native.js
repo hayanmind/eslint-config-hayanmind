191ce22f4cd787cb1136dab641f3cc6905e032cd
Daehoon Kim <ghsdh3409@gmail.com>
2022-11-14 13:14:28 +0900

    Add prettier (#7)
    * Add prettier
    
    * Add prettier

--- react-native.js
+++ react-native.js
@@ -1,14 +1,9 @@
 module.exports = {
   root: true,
-  extends: [
-    './index.js',
-    'plugin:react-native/all',
-  ],
-  plugins: [
-    'react-native',
-  ],
+  extends: ["./index.js", "plugin:react-native/all"],
+  plugins: ["react-native"],
   env: {
-    'react-native/react-native': true,
+    "react-native/react-native": true,
   },
   parserOptions: {
     ecmaFeatures: {
@@ -16,10 +11,10 @@ module.exports = {
     },
   },
   rules: {
-    'react-native/sort-styles': 'off',
-    'react-native/no-unused-styles': 'error',
-    'react-native/split-platform-components': 'error',
-    'react-native/no-inline-styles': 'error',
-    'react-native/no-color-literals': 'error',
+    "react-native/sort-styles": "off",
+    "react-native/no-unused-styles": "error",
+    "react-native/split-platform-components": "error",
+    "react-native/no-inline-styles": "error",
+    "react-native/no-color-literals": "error",
   },
 };
--- react.js
+++ react.js
@@ -1,10 +1,8 @@
 module.exports = {
   root: true,
-  extends: [
-    './index.js',
-  ],
+  extends: ["./index.js"],
   env: {
-    'browser': true,
+    browser: true,
   },
   parserOptions: {
     ecmaFeatures: {
@@ -12,7 +10,7 @@ module.exports = {
     },
     requireConfigFile: false,
     babelOptions: {
-      presets: ['@babel/preset-react'],
+      presets: ["@babel/preset-react"],
     },
   },
 };
