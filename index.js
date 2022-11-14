a0082dc23dfd47e0b4ecf5a257ab8c4a7364369e
Daehoon Kim <ghsdh3409@gmail.com>
2022-11-14 14:00:20 +0900

    Update eslint-config-airbnb-typescript

--- index.js
+++ index.js
@@ -86,6 +86,8 @@ module.exports = {
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "react/prop-types": "off",
         "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/default-param-last": "off",
+        "no-promise-executor-return": "off",
       },
     },
   ],
